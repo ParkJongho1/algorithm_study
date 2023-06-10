3d4dfaa923e0e5b3e2420a6ca7913d557a402af1
Parkjongho1 <godhojong1@gmail.com>
2023-06-10 17:05:40 +0900

    header work

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <Link style={{color: "red",}} href="/List"><div>go ListPage</div></Link>
-      <Link style={{color: "blue",}} href="/List"><div>go TestPage</div></Link>
+    <div style={{ marginTop: "10px" }}>
+      <div style={{ background: "red", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "blue", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "red", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "blue", height: 100, width: "100%" }}>a</div>
+
+      <div style={{ background: "red", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "blue", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "red", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "blue", height: 100, width: "100%" }}>a</div>
+      <div style={{ background: "red", height: 100, width: "100%" }}>a</div>
     </div>
-  )
+  );
 }
