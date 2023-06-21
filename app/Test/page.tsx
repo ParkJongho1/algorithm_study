0226182e7d81eaff848ad1a483062dbcdb96fe0f
Parkjongho1 <godhojong1@gmail.com>
2023-06-21 22:15:34 +0900

    @

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Test() {
-
   return (
     <div>
-        Test Page
-        <Link style={{color: "red",}} href="/List"><div>go ListPage</div></Link>
+      Test Page
+      <Link style={{ color: "red" }} href="/List">
+        <div>go ListPage1</div>
+      </Link>
     </div>
-  )
+  );
 }
