8f86d5bab30c7e59077b02d5bc4358db2e93e438
ParkJongho1 <godhojong1@gmail.com>
2023-07-25 09:46:34 +0900

    [One a day]  Programmers 0단계 두수의차

---
+++ programmers/javascript/lv0_sum.js
@@ -0,0 +1,9 @@
+// Programmers
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+// One a day! 0단계부터 하루에 하나이상!!
+
+function solution(num1, num2) {
+   var answer = 0;
+   answer = num1 - num2;
+   return answer;
+}
