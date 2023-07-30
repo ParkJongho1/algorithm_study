580fb058e04a059b691559c79f2390d437e8afc6
ParkJongho1 <godhojong1@gmail.com>
2023-07-31 08:04:49 +0900

    [One a day] Programmers 1단계 약수의 합

---
+++ programmers/javascript/lv_1/약수의합.js
@@ -0,0 +1,14 @@
+// Programmers
+// 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+// One a day! 0단계부터 하루에 하나이상!!!
+
+function solution(n) {
+   var answer = 0;
+   for (let i = 1; i <= n; i++) {
+      if (n % i === 0) {
+         answer += i;
+      }
+      // 삼항연산자: if( n % i === 0) { answer += i}
+   }
+   return answer;
+}
