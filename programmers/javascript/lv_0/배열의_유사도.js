23dd6ec130d873ff8ef47b07243d68dc89bddd35
ParkJongho1 <godhojong1@gmail.com>
2023-08-31 08:19:18 +0900

    [One a day] Programmers 0단계 배열 유사도

--- programmers/javascript/lv_0/배열의_유사도.js
+++ programmers/javascript/lv_0/배열의_유사도.js
@@ -1,5 +1,5 @@
 // Programmers
-// 두 배열이 얼마나 유사한지 확인해보려고 합니다. 문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요.
+// 두 배열이 얼마나 유사한지 확인해보려고 합니다. 문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요..
 // One a day! 0단계부터 하루에 하나이상!!!
 
 function solution(s1, s2) {
