36a817ca664df8ef9f5eb74fd47b039679c0ef94
ParkJongho1 <godhojong1@gmail.com>
2024-12-11 16:10:54 +0900

    [Oneday] Programmers 0단계 flag에 따라 다른 값 반환하기

---
+++ programmers/javascript/lv_0/flag에_따라_다른_값_반환.js
@@ -0,0 +1,7 @@
+// Programmers
+// 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+// One a day! 0단계부터 하루에 하나이상!!!
+
+function solution(a, b, flag) {
+  return flag ? a + b : a - b;
+}
