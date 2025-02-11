552527194ef8f00164a12aaf549499cda209de5a
ParkJongho1 <godhojong1@gmail.com>
2025-02-11 20:19:35 +0900

    [Oneday] Programmers 0단계 문자열을 정수로 변환하기

---
+++ programmers/javascript/lv_0/문자열을_정수로_변환하기.js
@@ -0,0 +1,12 @@
+// Programmers
+// 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+// One a day! 0단계부터 하루에 하나이상!!!
+
+function solution(n_str) {
+  return Number(n_str);
+}
+
+//다른사람 풀이
+function solution(n_str) {
+  return +n_str;
+}
