8e96b2e1d8a50ce785921906a5ed02268616adc4
ParkJongho1 <godhojong1@gmail.com>
2025-02-16 22:24:15 +0900

    [Oneday] Programmers 0단계 정수 부분

---
+++ programmers/javascript/lv_0/정수_부분.js
@@ -0,0 +1,10 @@
+// Programmers
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+// One a day! 0단계부터 하루에 하나이상!!!
+
+function solution(flo) {
+  return Math.trunc(flo);
+}
+
+//다른사람풀이
+const solution = (flo) => ~~flo;
